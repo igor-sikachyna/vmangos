f71518fe9a0875ce290e9f5beddb23b6fb07ac7c
Ihor Sikachyna <ihor.sikachyna@gmail.com>
2024-12-05 15:32:12 -0500

    Implemented basic in-memory damage scaling

--- src/game/LevelCraft/LevelCraft.cpp
+++ src/game/LevelCraft/LevelCraft.cpp
@@ -32,11 +32,60 @@ bool LevelCraft::LoadFromDB()
     return true;
 }
 
+void LevelCraft::HandleFlatAndPercentOutcomes(uint64 in_value, uint64 in_max, uint64& out_flat, uint64& out_pct, double& out_pctLow)
+{
+    out_flat += in_value;
+    double temp_pct = (in_value * 100.0) / in_max;
+    uint64 full_pct = temp_pct;
+    out_pct += full_pct;
+    out_pctLow += temp_pct - double(full_pct);
+    if (out_pctLow >= 1.0)
+    {
+        uint64 diff = out_pctLow;
+        out_pct += diff;
+        out_pctLow -= diff;
+    }
+}
+
 void LevelCraft::HandleMeleeOutcome(Unit* pVictim, CalcDamageInfo* damageInfo)
 {
+    // Ignore PvP
+    if (m_unit->IsPlayer() && pVictim->IsPlayer())
+        return;
+    // Ignore EvE
+    if (!m_unit->IsPlayer() && !pVictim->IsPlayer())
+        return;
+
     // m_unit is attacking pVictim
     if (pVictim->IsPlayer())
     {
-        Message(pVictim, "GUID: " + std::to_string(m_unit->GetGUID()));
+        uint32 entry = m_unit->GetEntry();
+        HandleFlatAndPercentOutcomes(
+            damageInfo->totalDamage,
+            pVictim->GetMaxHealth(),
+            m_combatExperience[entry].damageReceived,
+            m_combatExperience[entry].damageReceivedPct,
+            m_combatExperience[entry].temp_damageReceivedPctLow
+        );
+
+        /*Message(pVictim, "GUID: " + std::to_string(m_unit->GetGUID()));
+        Message(pVictim, "Entry: " + std::to_string(m_unit->GetEntry()));
+        Message(pVictim, "Area: " + std::to_string(pVictim->GetAreaId()));*/
+
+        damageInfo->totalDamage /= (1.0 + m_combatExperience[entry].damageReceivedPct / 100.0);
+    }
+    else if (m_unit->IsPlayer())
+    {
+        uint32 entry = pVictim->GetEntry();
+
+        HandleFlatAndPercentOutcomes(
+            damageInfo->totalDamage,
+            pVictim->GetMaxHealth(),
+            m_combatExperience[entry].damageDealt,
+            m_combatExperience[entry].damageDealtPct,
+            m_combatExperience[entry].temp_damageDealtPctLow
+        );
+
+        damageInfo->totalDamage *= (1.0 + m_combatExperience[entry].damageDealtPct / 100.0);
     }
 }
--- src/game/LevelCraft/LevelCraft.h
+++ src/game/LevelCraft/LevelCraft.h
@@ -3,23 +3,40 @@
 #ifndef MANGOSSERVER_LEVELCRAFT_H
 #define MANGOSSERVER_LEVELCRAFT_H
 
+#include <map>
+
 class Unit;
 struct CalcDamageInfo;
+
+struct CombatExperienceInfo
+{
+    uint64 damageDealt;
+    uint64 damageDealtPct;
+    uint64 damageReceived;
+    uint64 damageReceivedPct;
+
+    double temp_damageDealtPctLow;
+    double temp_damageReceivedPctLow;
+};
 
 class LevelCraft
 {
 public:
     LevelCraft(Unit* unit);
 
-    virtual bool Create();
+    bool Create();
 
-    virtual void SaveToDB();
-    virtual bool LoadFromDB();
+    void SaveToDB();
+    bool LoadFromDB();
 
-    virtual void HandleMeleeOutcome(Unit* pVictim, CalcDamageInfo* damageInfo);
+    void HandleMeleeOutcome(Unit* pVictim, CalcDamageInfo* damageInfo);
 
 private:
+    void HandleFlatAndPercentOutcomes(uint64 in_value, uint64 in_max, uint64& out_flat, uint64& out_pct, double& out_pctLow);
+
     Unit* m_unit;
+
+    std::map<uint32, CombatExperienceInfo> m_combatExperience;
 };
 
 #endif
